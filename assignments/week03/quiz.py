159899b0e69c2e915f37c1dce0f476b9d3a2e7d4
siradanaipholchai <siradanai.p@ku.th>
2026-07-31 09:28:40 +0700

    24quiz

--- assignments/week03/quiz.py
+++ assignments/week03/quiz.py
@@ -1,5 +1,5 @@
 # Complete this program to classify people by age
-age = int(input("Enter age: "))
+
 
 # Add your if-elif-else statements here
 # 0-12: Child
@@ -8,26 +8,41 @@ age = int(input("Enter age: "))
 # 60+: Senior
 
 # Your code here:
+age = int(input("Enter age: "))
+if age <= 12:
+    print("Child")
+elif age <= 19:
+    print("Teenager")
+elif age <= 59:
+    print("Adult")
+else:
+    print("Senior")
 
 
 
 # Complete this ATM simulation
+
+        
+        # Complete the menu logic here
+        # Your code here:
 balance = 1000
 pin = "1234"
-
+ 
 entered_pin = input("Enter PIN: ")
 if entered_pin == pin:
     print("PIN accepted")
     while True:
         print("\n1. Check Balance")
         print("2. Withdraw")
-        print("3. Deposit") 
+        print("3. Deposit")
         print("4. Exit")
-        
+       
         choice = input("Choose option: ")
-        
+       
         # Complete the menu logic here
         # Your code here:
-        
+       
 else:
     print("Invalid PIN")
+ 
+
