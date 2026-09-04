064b269fbc72c5de97f524701717e3962543037c
siradanaipholchai <siradanai.p@ku.th>
2026-09-04 10:09:44 +0700

    so sad kub today

---
+++ labs/week10-lab/example/check password.py
@@ -0,0 +1,11 @@
+# รับค่า password จากผู้ใช้
+# password นั้นแข็งแรงหรือไม่
+# password นั้นจะแข็งแรง ประกอบไปด้วยตัวเลข ตัวอักษร และ @ 1 ตัว ยาวมากกว่า 8 ตัว
+
+# ตัวอย่างหน้าจอ
+# Please input your password: Siradanai
+# Your password is not strong!
+
+# Please input your password: S1r@dana1
+# Your password is strong
+# และให้ระบบนับจำนวนตัวอักษร
---
+++ labs/week10-lab/example/count-letter.py
@@ -0,0 +1,18 @@
+# เขียนโปรแกรม
+# 1 รับค่าข้อความจากผู้ใช้ เป็นตัวแปร str ชื่อ text
+# 2 รับค่าอักขระที่ต้องการนับในข้อความ text
+# 3 ดำเนินการนับอักขระตามที่ผู้ใช้ต้องการ และแสดงออกทางหน้าจอ
+
+# ตัวอย่างหน้าจอ
+# Input your text : Boonchoo Jitnupong
+# Which character do you want to count: o
+# 5 Letter 'o' found in Boonchoo Jitnupong
+
+print("\n=== ITERATING THROUGH STRING ===")
+count = 0
+text = input('Input your text: ')
+x = input('Which character do you want to count: ')
+for letter in text:
+    if letter == :
+    count += 1
+    print(f"{count} letter 'l' found in '{text}'")
